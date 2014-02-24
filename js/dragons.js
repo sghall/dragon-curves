13f408b82eb5add9cd7a134efeae33cd1a34ad89
shall <hall.steven.g@gmail.com>
2014-02-23 23:26:31 -0300

    added buttons

--- js/dragons.js
+++ js/dragons.js
@@ -1,6 +1,6 @@
 (function (){
   var VIZ = {};
-  var width = 100, height = 100, segments = [];
+  var width = 100, height = 100, lines = [];
   var svg = d3.select("#svg-container")
     .append("svg")
     .attr("id", "thesvg")
@@ -12,12 +12,12 @@
   VIZ.count = 0;
 
   VIZ.addSegment = function (obj) {
-    segments.push(obj);
+    lines.push(obj);
   }
 
   VIZ.drawLines = function () {
     svg.selectAll(".lineSegment")
-      .data(segments, function (d) { return d.id; })
+      .data(lines, function (d) { return d.id; })
       .enter()
       .append("line")
       .attr("class", "lineSegment")
@@ -28,9 +28,9 @@
       .style("stroke", function (d) { return d.c; });
 
     svg.selectAll(".lineSegment")
-      .data(segments, function (d) { return d.id; })
+      .data(lines, function (d) { return d.id; })
       .exit()
-        .style("stroke", "grey")
+        .style("stroke", "#3399cc")
         .transition()
         .delay(500)
         .duration(500)
@@ -47,29 +47,30 @@
   }
 
   function findIntersection(p1, p2, p3, p4) {
-    var t1 = (p1.x * p2.y - p1.y * p2.x);
-    var t2 = (p3.x * p4.y - p3.y * p4.x);
-    var t3 = (p1.x - p2.x);
-    var t4 = (p3.y - p4.y);
-    var t5 = (p1.y - p2.y);
-    var t6 = (p3.x - p4.x);
-    var nx = (t1 * t6 - t3 * t2)/(t3 * t4 - t5 * t6);
-    var ny = (t1 * t4 - t5 * t2)/(t3 * t4 - t5 * t6);
+    var t1, t2, t3, t4, t5, t6, nx, ny;
+    t1 = (p1.x * p2.y - p1.y * p2.x);
+    t2 = (p3.x * p4.y - p3.y * p4.x);
+    t3 = (p1.x - p2.x);
+    t4 = (p3.y - p4.y);
+    t5 = (p1.y - p2.y);
+    t6 = (p3.x - p4.x);
+    nx = (t1 * t6 - t3 * t2)/(t3 * t4 - t5 * t6);
+    ny = (t1 * t4 - t5 * t2)/(t3 * t4 - t5 * t6);
     return {x: nx, y: ny};
   }
 
   VIZ.iterate = function () {
-    var len = segments.length, segment, p1, p2, mp, r1, r2, ip;
+    var len = lines.length, line, p1, p2, mp, r1, r2, ip;
     for (var i = 0; i < len; i++){
-      segment = segments.pop();
-      p1 = {x: segment.x1, y: segment.y1};
-      p2 = {x: segment.x2, y: segment.y2};
+      line = lines.pop();
+      p1 = {x: line.x1, y: line.y1};
+      p2 = {x: line.x2, y: line.y2};
       mp = {x: (p1.x + p2.x) / 2, y: (p1.y + p2.y) / 2};
       r1 = rotatePoint(mp, {x: p1.x, y: p1.y},  0.785398163);
       r2 = rotatePoint(mp, {x: p2.x, y: p2.y}, -0.785398163);
       ip = findIntersection(p1, r1, p2, r2);
-      segments.unshift({ id: _.uniqueId(), x1: p1.x, y1: p1.y, x2: ip.x, y2: ip.y, c: segment.c });
-      segments.unshift({ id: _.uniqueId(), x1: p2.x, y1: p2.y, x2: ip.x, y2: ip.y, c: segment.c });
+      lines.unshift({id: _.uniqueId(), x1: p1.x, y1: p1.y, x2: ip.x, y2: ip.y, c: line.c});
+      lines.unshift({id: _.uniqueId(), x1: p2.x, y1: p2.y, x2: ip.x, y2: ip.y, c: line.c});
     }
   }
 
